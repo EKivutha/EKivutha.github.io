ceaa11a50c7ba03b99004879b499c4519b940d85
EKivutha <30912874+EKivutha@users.noreply.github.com>
2022-01-24 04:30:57 +0000

    Deploying to gh-pages from  @ 367d582165d8c00ba5e051c6af0716db58e3bfc0 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f0b5402e01346af72ad0ffea8adf731.js"
+  "/portfolio_dev/precache-manifest.4e7068a4a2dd9c910d28a081157b8907.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_dev/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
