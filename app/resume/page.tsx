8fe9cc8cfcf07b69e1ff044ffb6492976be7598f
EKivutha <gerkibz@outlook.com>
2024-12-01 13:40:15 +0300

    update global styling

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,56 @@
+"use client";
+
+import { } from 'react-icons/fa';
+import { } from 'react-icons/si'
 import React from 'react'
 
+const about = {
+    title: '',
+    description: '',
+    info: [
+        {
+            fieldName: 'Experience',
+            fieldValue: '12 Years'
+        },
+        {
+            fieldName: 'Email',
+            fieldValue: 'mutunjau@gmail.com'
+        },
+        {
+            fieldName: 'Skype',
+            fieldValue: '12 Years'
+        },
+        {
+            fieldName: 'Nationality',
+            fieldValue: 'Kenyan'
+        },
+        {
+            fieldName: 'Languages',
+            fieldValue: 'English'
+        },
+    ]
+}
+const eperience = {
+    title: '',
+    description: '',
+    info: [
+        {
+            company:'Stanbic Bank',
+            position:'Developer Operation II',
+            duration:'2023-Present'
+        },
+        {
+            company:'Adanian Labs',
+            position:'',
+            duration:''
+        },
+        {
+            company:'',
+            position:'',
+            duration:''
+        },
+    ]
+}
 const Resume = () => {
     return (
         <div>
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,10 +1,91 @@
+
+"use client";
 import React from 'react'
+
+import Link from "next/link"
+import { BsArrowDownRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+
+const services = [
+  {
+    num: '01',
+    title: 'Web development',
+    descrption: 'Solidity, Rust, Nextjs, Reactjs, Tailwind',
+    href: ''
+  },
+  {
+    num: '02',
+    title: 'Mobile Development',
+    descrption: 'Flutter, React Native',
+    href: ''
+  },
+  {
+    num: '03',
+    title: 'Data science',
+    descrption: 'Python, Pytorch, Keras',
+    href: ''
+  },
+  {
+    num: '04',
+    title: 'Project Management',
+    descrption: 'Schedule, Budgeting',
+    href: ''
+  },
+]
 
 const Services = () => {
   return (
-    <div>
-      services page
-    </div>
+    <section className='min-h-[80vh] 
+            flex flex-col 
+            justify-center 
+            py-12 
+            xl:py0'>
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: {
+              delay: 2.4,
+              duration: 0.4,
+              ease: "easeIn"
+            }
+          }}
+          className='grid grid-cols-1 md:grid-cols-2 gap-[60px]'>
+          {services.map((service, index) => {
+            return (
+              <div key={index}
+                className='flex-1 flex flex-col justify-center gap-6 group'>
+                <div className='flex w-full justify-between items-center'>
+                  <div
+                    className='text-5xl font-extrabold text-outline
+                    text-transparent group-hover:text-outline-hover
+                    transition-all duration-500'>
+                    {service.num}
+                  </div>
+                  <Link href={service.href}
+                    className='w-[70px] h-[70px] rounded-full
+                  bg-white group-hover:bg-accent transition-all
+                  duration-500 flex justify-center items-center hover:-rotate-45'>
+                    <BsArrowDownRight
+                      className='text-3xl text-primary' />
+                  </Link>
+                </div>
+                {/* title */}
+                <h2 className='text-[42px] font-bold leading-none
+                text-white group-hover:text-accent 
+                transiton-all duration-500'>{service.title}</h2>
+                {/* description */}
+                <p
+                className='text-white/60 '>{service.descrption}</p>
+                {/* border */}
+                <div className="border border-white/20 w-full" />
+              </div>
+            )
+          })}
+        </motion.div>
+      </div>
+    </section>
   )
 }
 
