96bad7e2fd6e58ff44e40e4a8efe96477748ce61
EKivutha <gerkibz@outlook.com>
2025-05-03 21:15:37 +0300

    update job responsibilities

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -35,19 +35,67 @@ const eperience = {
     description: '',
     info: [
         {
-            company:'Stanbic Bank',
-            position:'Developer Operation II',
-            duration:'2023-Present'
+            company: 'Stanbic Bank',
+            position: 'Developer Operation II',
+            duration: '2023-Present',
+            responsibilities : "\
+            Developed and maintained web applications using React.js, Node.js, and Express to enhance user experience and performance.\
+            Collaborated with cross-functional teams to define and implement new features, resulting in a 20% increase in user engagement.\
+            Optimized codebase for efficiency, reducing load times by 30% through refactoring and implementing caching strategies.\
+            Implemented CI/CD pipelines using Jenkins and Docker, improving deployment frequency and reducing downtime.\
+            Conducted code reviews and provided constructive feedback to peers, fostering a culture of continuous improvement.\
+            Led a team of junior developers, mentoring them on best practices and contributing to their professional growth."        
         },
         {
-            company:'Adanian Labs',
-            position:'',
-            duration:''
+            company: 'Adanian Labs',
+            position: '',
+            duration: '',
+            responsibilities: "\
+            Design, develop, and implement blockchain solutions, including smart contracts and decentralized applications (DApps).\
+            Collaborate with cross-functional teams to define and document project requirements.\
+            Ensure the security and integrity of blockchain systems.\
+            Write, test, and deploy smart contracts on various blockchain platforms such as Ethereum, Binance Smart Chain, and others.\
+            Audit and optimize existing smart contracts for security and efficiency.\
+            Stay updated with the evolving technology landscape and collaborate closely with stakeholders to align blockchain solutions with business goals.\
+            "
         },
         {
-            company:'',
-            position:'',
-            duration:''
+            company: 'Kibz Tech',
+            position: '',
+            duration: '',
+            responsibilities: '\
+            Founded and grew InnovativeTech Solutions from a startup to a leading provider of cutting-edge software solutions, generating over $10 million in annual revenue.\
+            Spearheaded the development of multiple successful products, including a revolutionary AI-powered analytics platform that significantly improved client decision-making and operational efficiency.\
+            Led a diverse team of engineers, designers, and marketers, fostering a collaborative and innovative culture that drove continuous growth and success.\
+            Secured Series A funding of $5 million, enabling rapid expansion and scaling of operations.\
+            Established strategic partnerships with key industry players, enhancing market presence and driving new business opportunities.\
+            Implemented agile methodologies and best practices, optimizing product development cycles and reducing time-to-market by 30%.\
+            Mentored and guided junior team members, promoting professional development and ensuring high performance across the organization.\
+            Actively engaged with the tech community through speaking engagements, webinars, and industry conferences, positioning the company as a thought leader in the tech space.'
+        },
+        {
+            company: 'Omdena',
+            position: '',
+            duration: '',
+            responsibilities: '\
+            Designed and implemented machine learning models to predict customer behavior, leading to a 15% increase in sales and 20% improvement in customer retention.\
+            Developed and maintained data pipelines and ETL processes using Python and Apache Spark, ensuring data integrity and availability for analysis.\
+            Collaborated with cross-functional teams to understand business requirements and provide data-driven insights, enhancing decision-making processes.\
+            Performed exploratory data analysis (EDA) using Pandas, NumPy, and Matplotlib, uncovering actionable insights that drove strategic business initiatives.\
+            Deployed predictive models using AWS and Docker, automating workflows and improving operational efficiency.\
+            Led data science projects from ideation to implementation, mentoring junior data scientists and fostering a culture of continuous learning and innovation.'
+        },
+        {
+            company: 'Eprod Solutions',
+            position: '',
+            duration: '',
+            responsibilities : "\
+            Developed and maintained web applications using React.js, Node.js, and Express to enhance user experience and performance.\
+            Collaborated with cross-functional teams to define and implement new features, resulting in a 20% increase in user engagement.\
+            Optimized codebase for efficiency, reducing load times by 30% through refactoring and implementing caching strategies.\
+            Implemented CI/CD pipelines using Jenkins and Docker, improving deployment frequency and reducing downtime.\
+            Conducted code reviews and provided constructive feedback to peers, fostering a culture of continuous improvement.\
+            Led a team of junior developers, mentoring them on best practices and contributing to their professional growth."
         },
     ]
 }
